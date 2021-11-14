7a279869f5532dc9807fdbf846f49ff316842799
Ezequiel Pássaro <epassaro15@gmail.com>
2021-11-14 00:58:48 -0300

    first deploy

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # docstr-cov-workflow
+![docstr-cov](https://img.shields.io/endpoint?url=https://jsonbin.org/epassaro/docstr-cov-workflow/badges/docstr-cov)
+
 Measure docstring coverage of Python packages with GitHub Actions
---
+++ example/base.py
@@ -0,0 +1,17 @@
+def add(a,b):
+    """
+    Add two numbers
+    """
+    return a+b
+
+def sub(a,b):
+    """
+    Substract two numbers
+    """
+    return a-b
+
+def mult(a,b):
+    return a*b
+    
+def div(a,b):
+    return a/b
